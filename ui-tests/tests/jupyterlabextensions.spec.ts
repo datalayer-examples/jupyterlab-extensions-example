83c332ce30cb44027aa7d31d24453d956ad7457b
Eric Charles <eric@datalayer.io>
2022-11-30 12:36:10 +0100

    lint

--- ui-tests/tests/jupyterlabextensions.spec.ts
+++ ui-tests/tests/jupyterlabextensions.spec.ts
@@ -16,8 +16,6 @@ test('should emit an activation console message', async ({ page }) => {
   await page.goto();
 
   expect(
-    logs.filter(
-      s => s === 'jupyterlab_apod_left is activated.'
-    )
+    logs.filter(s => s === 'jupyterlab_apod_left is activated.')
   ).toHaveLength(1);
 });
