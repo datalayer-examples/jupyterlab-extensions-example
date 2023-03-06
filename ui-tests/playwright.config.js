6ce9456cdd8943524dc81b8011717c3dc4c63ed8
Eric Charles <eric@datalayer.io>
2023-03-06 18:11:30 +0100

    chore: test config

--- ui-tests/playwright.config.js
+++ ui-tests/playwright.config.js
@@ -1,14 +1,41 @@
-/**
- * Configuration for Playwright using default from @jupyterlab/galata
- */
-const baseConfig = require('@jupyterlab/galata/lib/playwright-config');
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+
+var baseConfig = require('@jupyterlab/galata/lib/playwright-config');
 
 module.exports = {
   ...baseConfig,
-  webServer: {
-    command: 'jlpm start',
-    url: 'http://localhost:8888/lab',
-    timeout: 120 * 1000,
-    reuseExistingServer: !process.env.CI
-  }
+  projects: [
+    {
+      name: 'documentation',
+      // Try one retry as some tests are flaky
+      retries: process.env.CI ? 2 : 0,
+      testMatch: 'test/documentation/**',
+      testIgnore: '**/.ipynb_checkpoints/**',
+      timeout: 90000,
+      use: {
+        contextOptions: {
+          permissions: ['clipboard-read', 'clipboard-write']
+        },
+        launchOptions: {
+          // Force slow motion
+          slowMo: 30
+        }
+      }
+    },
+    {
+      name: 'galata',
+      testMatch: 'ui-tests/galata/**',
+      testIgnore: '**/.ipynb_checkpoints/**'
+    },
+    {
+      name: 'jupyterlab',
+      testMatch: 'test/jupyterlab/**',
+      testIgnore: '**/.ipynb_checkpoints/**'
+    }
+  ],
+  // Switch to 'always' to keep raw assets for all tests
+  preserveOutput: 'failures-only', // Breaks HTML report if use.video == 'on'
+  // Try one retry as some tests are flaky
+  retries: process.env.CI ? 1 : 0
 };
